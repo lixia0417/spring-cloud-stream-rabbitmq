485f5b0b6ddfadde959e6f20cf8015348e823fad
buxiaoxia <buxiaoxia@126.com>
2017-03-10 14:45:52 +0800

    spring-cloud hystrix使用

--- spring-cloud-netflix/spring-cloud-hystrix/README.md
+++ spring-cloud-netflix/spring-cloud-hystrix/README.md
@@ -2,6 +2,8 @@
 
 ##hystrix官方使用文档
 
+https://github.com/Netflix/Hystrix/wiki/How-To-Use
+
 https://github.com/Netflix/Hystrix/tree/master/hystrix-contrib/hystrix-javanica#configuration
 
 ##hystrix官方配置文档
--- spring-cloud-netflix/spring-cloud-hystrix/src/main/java/com/buxiaoxia/business/rest/HelloController.java
+++ spring-cloud-netflix/spring-cloud-hystrix/src/main/java/com/buxiaoxia/business/rest/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 /**
  * Created by xw on 2017/3/6.
  * 2017-03-06 13:52
@@ -18,8 +21,14 @@ public class HelloController {
 	private HelloService helloService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String getHello(){
+	public String getHello() {
 		return helloService.getHello();
+	}
+
+	@RequestMapping(value = "/async", method = RequestMethod.GET)
+	public String getHelloAsync() throws ExecutionException, InterruptedException {
+		Future<String> future = helloService.getHelloAsync();
+		return future.get();
 	}
 
 }
