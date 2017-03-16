1b3891dbddb3c729167b5c7cfd30c2f842122555
buxiaoxia <buxiaoxia@126.com>
2017-03-16 23:55:24 +0800

    spring-boot-oauth2-server 认证服务器

--- spring-oauth2/spring-boot-oauth-server/src/main/java/com/buxiaoxia/system/config/OAuth2AuthorizationServerConfig.java
+++ spring-oauth2/spring-boot-oauth-server/src/main/java/com/buxiaoxia/system/config/OAuth2AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -16,6 +17,9 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
 import java.util.concurrent.TimeUnit;
@@ -110,4 +114,21 @@ public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigur
 	public TokenEnhancer tokenEnhancer() {
 		return new CustomTokenEnhancer();
 	}
+
+
+
+//    @Bean
+//    public TokenStore tokenStore() {
+//    return new JwtTokenStore(accessTokenConverter());
+//    }
+//
+//    @Bean
+//    public JwtAccessTokenConverter accessTokenConverter() {
+//    final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//    // converter.setSigningKey("123");
+////    final KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("mytest.jks"), "mypass".toCharArray());
+////    converter.setKeyPair(keyStoreKeyFactory.getKeyPair("mytest"));
+//    return converter;
+//    }
+
 }
