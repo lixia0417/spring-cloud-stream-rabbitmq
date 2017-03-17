412e0f7aeaaee2103efef699218c6d7143d7f9b5
buxiaoxia <buxiaoxia@126.com>
2017-03-17 14:38:44 +0800

    spring-boot-oauth2-server 认证服务器

--- spring-oauth2/spring-boot-oauth-server/src/main/java/com/buxiaoxia/Application.java
+++ spring-oauth2/spring-boot-oauth-server/src/main/java/com/buxiaoxia/Application.java
@@ -2,15 +2,32 @@ package com.buxiaoxia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * Created by xw on 2017/2/20.
  * 2017-02-20 16:51
  */
+@RestController
 @SpringBootApplication
 @EnableAuthorizationServer
 public class Application {
+
+	@RequestMapping(value = "/user",method = RequestMethod.GET)
+	public ResponseEntity getUser(){
+		return new ResponseEntity<>(new HashMap<String,String>(){{
+			put("name","xw");
+			put("loginName","buxiaoxia");
+		}}, HttpStatus.OK);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
