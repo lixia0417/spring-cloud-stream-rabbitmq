8356d3b37e91f26fb7da7060374c8a9db3219de9
buxiaoxia <buxiaoxia@126.com>
2017-03-20 00:07:13 +0800

    spring-boot-oauth2-jwt-resource 服务

---
+++ spring-oauth2/spring-boot-oauth-jwt-resource-server/src/main/java/com/buxiaoxia/Application.java
@@ -0,0 +1,38 @@
+package com.buxiaoxia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by xw on 2017/2/20.
+ * 2017-02-20 16:51
+ */
+@RestController
+@SpringBootApplication
+public class Application {
+
+
+	@RequestMapping(value = "/auth", method = RequestMethod.GET)
+	public ResponseEntity authDemo(OAuth2Authentication auth) {
+		// 获取当前用户资源
+		return new ResponseEntity<>(new HashMap<String, Object>() {{
+			put("username", auth.getPrincipal());
+			put("auth", "OK");
+		}}, HttpStatus.OK);
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
+}
